563e06d8a2876571ba03cfb8ee02e840f736fb6a
Tirantbacon <dylzam8@gmail.com>
2017-08-16 21:12:18 +1000

    Initial commit

---
+++ escape_sequences.py
@@ -0,0 +1,8 @@
+#  Example of using a single quote escape sequence
+print('How much wood can it\'s wood chuck chuck?')
+#  Example of using a triple apostrophe escape sequence (not technically an escape sequence but can be helpful)
+print('''How much wood can it's wood chuck chuck?''')
+#  Example of using a escape sequence within a double quote within a double quote.
+print("How much wood can it's \"wood\" chuck chuck?")
+#  Example of using a double backslash escape sequence
+print("Running \\wood, wood executed!")
